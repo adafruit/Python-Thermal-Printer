3fc03d8cc13e4b4ff5620b87d01f7e51daf4cd56
Phillip Burgess <paintyourdragon@dslextreme.com>
2013-06-25 09:53:54 -0700

    Twitter search script now works with 1.1 API
    Requires Twitter account and some developer setup. See comments in
    twitter.py for guidance.

--- twitter.py
+++ twitter.py
@@ -4,44 +4,105 @@
 # Polls one or more Twitter accounts for changes, displaying updates
 # on attached thermal printer.
 # Written by Adafruit Industries.  MIT license.
-# 
+#
 # Required hardware includes an Internet-connected system with Python
 # (such as Raspberry Pi) and an Adafruit Mini Thermal Receipt printer
 # and all related power supplies and cabling.
 # Required software includes Adafruit_Thermal and PySerial libraries.
-# 
+#
 # Resources:
 # http://www.adafruit.com/products/597 Mini Thermal Receipt Printer
 # http://www.adafruit.com/products/600 Printer starter pack
+#
+# Uses Twitter 1.1 API application-only authentication.  This
+# REQUIRES a Twitter account and some account configuration.  Start
+# at dev.twitter.com, sign in with your Twitter credentials, select
+# "My Applications" from the avatar drop-down menu at the top right,
+# then "Create a new application."  Provide a name, description,
+# placeholder URL and complete the captcha, after which you'll be
+# provided a "consumer key" and "consumer secret" for your app.
+# Copy these strings to the globals below, and configure the search
+# string to your liking.  DO NOT SHARE your consumer key or secret!
+# If you put code on Github or other public repository, replace them
+# with dummy strings.
 
 from __future__ import print_function
-import sys, urllib, json, time, HTMLParser
+import base64, HTMLParser, httplib, json, sys, urllib, zlib
 from unidecode import unidecode
 from Adafruit_Thermal import *
 
+
+# Configurable globals.  Edit to your needs. -------------------------------
+
+# Twitter application credentials -- see notes above -- DO NOT SHARE.
+consumer_key    = 'PUT_YOUR_CONSUMER_KEY_HERE'
+consumer_secret = 'PUT_YOUR_CONSUMER_SECRET_HERE'
+
 # queryString can be any valid Twitter API search string, including
-# boolean operators.  See https://dev.twitter.com/docs/using-search
+# boolean operators.  See http://dev.twitter.com/docs/using-search
 # for options and syntax.  Funny characters do NOT need to be URL
 # encoded here -- urllib takes care of that.
 queryString = 'from:Adafruit'
 
-# These shouldn't need to be changed:
-serverName  = 'search.twitter.com'
-printer     = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
+
+# Other globals.  You probably won't need to change these. -----------------
+
+printer   = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
+host      = 'api.twitter.com'
+authUrl   = '/oauth2/token'
+searchUrl = '/1.1/search/tweets.json?'
+agent     = 'Gutenbird v1.0'
 # lastID is command line value (if passed), else 1
 if len(sys.argv) > 1: lastId = sys.argv[1]
 else:                 lastId = '1'
 
-url    = ('http://search.twitter.com/search.json?' +
-          urllib.urlencode(dict(q=queryString)) +
-          '&rpp=3&since_id=' + lastId)
-data   = json.load(urllib.urlopen(url))
-lastId = data['max_id_str']
+# Initiate an HTTPS connection/request, uncompress and JSON-decode results
+def issueRequestAndDecodeResponse(method, url, body, headers):
+  connection = httplib.HTTPSConnection(host)
+  connection.request(method, url, body, headers)
+  response = connection.getresponse()
+  if response.status != 200:
+    print('HTTP error: %d' % response.status)
+    exit(-1)
+  compressed = response.read()
+  connection.close()
+  return json.loads(zlib.decompress(compressed, 16+zlib.MAX_WBITS))
 
-for tweet in data['results']:
+
+# Mainline code. -----------------------------------------------------------
+
+# Get access token. --------------------------------------------------------
+
+token = issueRequestAndDecodeResponse(
+  'POST', authUrl, 'grant_type=client_credentials',
+   {'Host'            : host,
+    'User-Agent'      : agent,
+    'Accept-Encoding' : 'gzip',
+    'Content-Type'    : 'application/x-www-form-urlencoded;charset=UTF-8',
+    'Authorization'   : 'Basic ' + base64.b64encode(
+     urllib.quote(consumer_key) + ':' + urllib.quote(consumer_secret))}
+  )['access_token']
+
+
+# Perform search. ----------------------------------------------------------
+
+data = issueRequestAndDecodeResponse(
+  'GET',
+  (searchUrl + 'count=3&since_id=%s&q=%s' %
+   (lastId, urllib.quote(queryString))),
+  None,
+  {'Host'            : host,
+   'User-Agent'      : agent,
+   'Accept-Encoding' : 'gzip',
+   'Authorization'   : 'Bearer ' + token})
+
+
+# Display results. ---------------------------------------------------------
+
+for tweet in data['statuses']:
 
   printer.inverseOn()
-  printer.print(' ' + '{:<31}'.format(tweet['from_user']))
+  printer.print(' ' + '{:<31}'.format(tweet['user']['screen_name']))
   printer.inverseOff()
 
   printer.underlineOn()
@@ -55,4 +116,4 @@ for tweet in data['results']:
 
   printer.feed(3)
 
-print(lastId) # Piped back to calling process
+print(data['search_metadata']['max_id_str']) # Piped back to calling process
